30bb9b0b8f56a002cf461f693f9e254fece8b4dd
scarchik <scarchik@gmail.com>
2017-08-11 18:24:48 +0300

    Allow render operation summary as Markdown

--- src/core/components/operation.jsx
+++ src/core/components/operation.jsx
@@ -127,7 +127,7 @@ export default class Operation extends PureComponent {
 
             { !showSummary ? null :
                 <div className="opblock-summary-description">
-                  { summary }
+                  <Markdown source={ summary } />
                 </div>
             }
 
